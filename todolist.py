74a79bfbe4de28d2ecf186f570fe1d9298c8de1d
Biddybuggy <100064835+Biddybuggy@users.noreply.github.com>
2022-07-08 10:29:24 +0700

    Update todolist.py

--- todolist.py
+++ todolist.py
@@ -1,19 +1,42 @@
-mytasks = []
-current = int(input("What would you like to do? \n1. Add a task \n2. Remove a task \n3. View tasks \n4. Exit \nInput: "))
+import json
 
-while current != 4:
+mylist = []
+current = int(input("What would you like to do? \n1. Add a task \n2. Remove or complete a task \n3. View your to-do list \n4. Save your tasks \n5. Quit \nInput: "))
+while current != 5:
     if current == 1:
-        taskname = input("Enter your task name: ")
-        duedate = input("Enter due date (Day or date, time): ")
-        mytasks.append(f"{taskname} (Due date: {duedate})")
+        task = input("\nEnter task name: ")
+        duedate = input("Enter due date: ")
+        mylist.append(f"{task} (Due date: {duedate})")
+        
     elif current == 2:
-        print(mytasks)
-        numberdel = int(input("Which task number would you like to delete? "))
-        mytasks.pop(numberdel - 1)
-    elif current == 3:
-        print(f"Your tasks: {mytasks}")
-    else:
-        break
-    current = int(input("What would you like to do? \n1. Add a task \n2. Remove a task \n3. View tasks \n4. Exit \nInput: "))
+        if mylist == []:
+            print("\nYour To-Do List is empty!")
+        else:
+            print("\nYour Tasks")
+            for each_item in mylist:
+                print(each_item)
+            removedtask = int(input("Which task would you like to remove or complete (enter task's number from the top): "))
+            mylist.pop(removedtask - 1)
 
-print("Looking forward to keeping more of your tasks!")
+    elif current == 3:
+        if mylist == []:
+            print("\nWoohoo! You don't have any tasks to do!")
+        else:
+            print("\nYour Tasks")
+            for each_item in mylist:
+                print(each_item)
+
+    elif current == 4:
+        print("\nSaving your tasks!")
+        jsonList = json.dumps(mylist)
+        jsonFile = open("path","w")
+        jsonFile.write(jsonList)
+        jsonFile.close()
+        print("\nYour tasks have been saved!")
+        
+    elif current < 1 or current > 5:
+        print("\nInvalid Input!")
+
+    current = int(input("\nWhat would you like to do? \n1. Add a task \n2. Remove or complete a task \n3. View your to-do list \n4. Save your tasks \n5. Quit \nInput: "))
+
+print("\nThanks for using To-Do List!")
